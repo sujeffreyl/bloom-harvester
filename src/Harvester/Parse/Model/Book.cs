d58e092b7761e16c676d38f0438b068d1769962c
John Hatton <hattonjohn@gmail.com>
2019-07-24 09:37:59 -0600

    add comment

--- src/Harvester/Parse/Model/Book.cs
+++ src/Harvester/Parse/Model/Book.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,30 +10,30 @@ namespace BloomHarvester.Parse.Model
 	{
 		// TODO: There are many more properties from the book table that we could add when they are needed.
 
-		[JsonProperty("baseUrl")]
-		public string BaseUrl;
+        [JsonProperty("baseUrl")]
+        public string BaseUrl;
 
-		[JsonProperty("warnings")]
-		public List<string> Warnings;
+        [JsonProperty("warnings")]
+        public List<string> Warnings;
 
-		public const string kHarvestStateField = "harvestState";
-		public const string kHarvesterIdField = "harvesterId";
-		public const string kWarningsField = "warnings";
+        public const string kHarvestStateField = "harvestState";
+        public const string kHarvesterIdField = "harvesterId";
+        public const string kWarningsField = "warnings";
 
-		// Returns the class name (like a table name) of the class on the Parse server that this object corresponds to
-		internal override string GetParseClassName()
-		{
-			return "books";
-		}
+        // Returns the class name (like a table name) of the class on the Parse server that this object corresponds to
+        internal override string GetParseClassName()
+        {
+            return "books";
+        }
 
-		public enum HarvestState
-		{
-			Unknown,
-			New,
-			Updated,
-			InProgress,
-			Done,
-			Failed,
-		}
-	}
+        public enum HarvestState
+        {
+            Unknown,
+            New,
+            Updated, // set by parse code when the user re-uploads a book
+            InProgress,
+            Done,
+            Failed,
+        }
+    }
 }
